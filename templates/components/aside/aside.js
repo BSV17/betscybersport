09838f43371516e836af7271e31e89c59e37b93a
BSV17 <91079032+BSV17@users.noreply.github.com>
2022-09-08 11:15:49 +0300

    fix(aside): updated aside navigation and minor fixes

---
+++ templates/components/aside/aside.js
@@ -0,0 +1,37 @@
+const asideNavigationList = document.querySelector('.aside-navigation > ul');
+const asideSection = document.querySelector('.section__aside');
+const asideSectionContent = asideSection.nextElementSibling;
+const contentHeadings = asideSectionContent.querySelectorAll('h3');
+
+for (let i = 0; i < contentHeadings.length; i++) {
+    contentHeadings[i].id = `heading-${i + 1}`;
+    let listItem = document.createElement('li');
+    listItem.classList.add('aside-navigation__item');
+    let itemLink = document.createElement('a');
+    itemLink.href = `#heading-${i + 1}`;
+    itemLink.innerHTML = contentHeadings[i].textContent;
+    listItem.appendChild(itemLink);
+    asideNavigationList.appendChild(listItem);
+}
+
+const navigationItems = document.querySelectorAll('.aside-navigation__item');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            navigationItems.forEach((link) => {
+                const navigationLink = link.querySelector('a');
+                link.classList.toggle(
+                    'aside-navigation__item_active',
+                    navigationLink.getAttribute('href').replace('#', '') === entry.target.id
+                );
+            });
+        }
+    });
+}, {
+    threshold: 1,
+});
+
+contentHeadings.forEach(
+    (target) => observer.observe(target)
+);
